dd8590f569a9d613344da0aa9dc48d40e6088413
inigodedios <6403284142001581>
2020-11-19 20:44:31 +0100

    Apuntes BD

--- Apuntes/src/BD/BDOperaciones.java
+++ Apuntes/src/BD/BDOperaciones.java
@@ -22,6 +22,7 @@ public class BDOperaciones {
             st = con.createStatement(); //Creo el objeto sentencia
             ResultSet rs = st.executeQuery(sql); //Ejecutamos la consulta
             if(rs.next()) { //rs.next() -> Devuelve true si rs tiene datos, false en caso contrario
+                rs.getString("con");
                 existe = true;
             }
             cerrarBD(con, st);
@@ -30,5 +31,19 @@ public class BDOperaciones {
             e.printStackTrace();
         }
         return existe;
+    }
+
+    public static void insertarUsuario(String nick, String con) {
+
+        String s = "INSERT INTO Usuario VALUES('"+nick+"','"+con+"')";
+        Connection c = initBD("academia.db");
+        try {
+            Statement st = c.createStatement();
+            st.executeUpdate(s);
+            cerrarBD(c, st);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
