e3e44e406e9e642af8e657401bb8eb897664735c
inigodedios <6403284142001581>
2020-11-19 19:36:35 +0100

    d

--- Apuntes/src/BD/BDConceptos.java
+++ Apuntes/src/BD/BDConceptos.java
@@ -1,4 +1,15 @@
 package BD;
 
+import javax.swing.*;
+
 public class BDConceptos {
+/*
+1. ESTABLECER CONEXIÓN CON LA BD
+2. CREAR UN OBJETO STATETMENT
+3. EJECUTAR SENTENCIA SQL
+4. LEER EL RESULSET
+ */
+
+
+
 }
