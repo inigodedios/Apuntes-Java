73bafe182d18dcf1cc40e9d44ee898dcfed36d38
inigodedios <6403284142001581>
2020-11-19 23:43:32 +0100

    .

--- Apuntes/src/EscrituraEnFicheros/FicherosBinarios.java
+++ Apuntes/src/EscrituraEnFicheros/FicherosBinarios.java
@@ -1,4 +1,63 @@
 package EscrituraEnFicheros;
 
+import java.io.*;
+import java.util.TreeSet;
+
 public class FicherosBinarios {
+
+    public static void main(String[] args) {
+
+        String ts = "prueba";
+
+        //ESCRIBIR EN UN FICHERO BINARIO
+        ObjectOutputStream oos = null;
+
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream("Coches.dat"));
+            oos.writeObject(ts);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) { //TODO
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.flush();
+                    oos.close();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        //LEER UN FICHERO BINARIO
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(new FileInputStream("Coches.dat"));
+            TreeSet<Object> tsDelFichero = (TreeSet<Object>) ois.readObject();
+            System.out.println("El número de coches almacenados en el fichero es: " + tsDelFichero.size());
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            if (ois != null) {
+                try {
+                    ois.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
+
--- Apuntes/src/EscrituraEnFicheros/FicherosDeTexto.java
+++ Apuntes/src/EscrituraEnFicheros/FicherosDeTexto.java
@@ -1,4 +1,51 @@
 package EscrituraEnFicheros;
 
+import java.io.*;
+
 public class FicherosDeTexto {
+    public static void main(String[] args) {
+        //ESCRIBIR EN UN FICHERO DE TEXTO
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter("Ejemplo.txt");
+            pw.println("Hola");
+            pw.println("vamos a crear un fichero de texto");
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace(); //Muestra mensaje de error
+        } finally {
+            if (pw != null) {
+                pw.flush();
+                pw.close();
+            }
+        }
+
+        //LEER DE UN FICHERO DE TEXTO
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader("Ejemplo.txt"));
+            String linea = br.readLine();
+            while (linea != null) {
+                System.out.println(linea);
+                linea = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
