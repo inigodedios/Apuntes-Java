eebcfcc5a4a4f738a677485a76f2f76a3a1a5321
inigodedios <6403284142001581>
2020-12-07 01:55:14 +0100

    Hilos.java Ejemplos cambio de color

--- Apuntes/src/Layout/BoxLayout.java
+++ Apuntes/src/Layout/BoxLayout.java
@@ -2,4 +2,10 @@ package Layout;
 
 public class BoxLayout {
 //    TODO
+    /*Conversion de String a fecha
+    dateFormatEjemplo.parse (String s);
+
+    Formateo de fecha a String
+    dateFormatEjemplo.format (Date d);
+     */
 }
