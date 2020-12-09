15db518176b0709d0bb06a193cb8461e2083ee61
inigodedios <6403284142001581>
2020-12-09 18:19:53 +0100

    JTable

--- Apuntes/src/Componentes/JTable.java
+++ Apuntes/src/Componentes/JTable.java
@@ -1,7 +1,30 @@
 package Componentes;
 
+
+import javax.swing.table.DefaultTableModel;
+
 public class JTable {
+    /*Existen dos formas de crear tablas, usando una matriz de datos o mediante DefaultTableModel.
+    *Nosotros vamos a usar siempre el DefaultTableModel
+    */
+
+    private JTable tabla;
+    private DefaultTableModel tModel; //Modelo de la tabla
+
 //    TODO
     //Cambio de color
     //mirar examen 11 de dicembre 2020 codigo de preparatorio
+
+    private void mostrarDatosConTableModel (){
+        tabla = new JTable();
+        tModel = new DefaultTableModel(); //Definimos el objeto tableModel
+        tabla.setModel(tModel); //NO se porque da error, pero esta bien. Se importa mal
+        tModel.addColumn("Nombre");
+        tModel.addColumn("Apellido");
+        tModel.addColumn("DNI");
+
+        tabla.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        tabla.getTableHeader().setReorderingAllowed(false);
+
+    }
 }
